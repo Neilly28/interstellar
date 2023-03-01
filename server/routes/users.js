1efcd9b86a5f8635b7aee20d327404005467cbcc
Neilly <neilpilarca@gmail.com>
2023-03-01 17:57:35 +0100

    add verify token

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,13 +1,29 @@
 import express from "express";
+import { verifyToken } from "../utils/verifyToken.js";
+
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("hello from auth endpoitn");
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+  res.send("hello user you are logged in!!!");
 });
 
-router.get("/register", (req, res) => {
-  res.send("hello this is register ednpoint!");
-});
+// update
+router.put("/:id", updateUser);
+
+// delete
+router.delete("/:id", deleteUser);
+
+// get
+router.get("/:id", getUser);
+
+// get all
+router.get("/", getUsers);
 
 export default router;
